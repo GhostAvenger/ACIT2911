c3f8b1866455e6d91edabd590b68d2f4ae42b4de
Jimmy Truong <jtruong20@my.bcit.ca>
2019-05-16 11:03:07 -0700

    2019-05-16

--- ACIT2911_PROJECT/python_core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def login(request):
-    return render(request,"login_test.html")
-def register(request):
-    return render(request,"register_test.html")
